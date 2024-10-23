c809ed982a99e9d62eb4dc30aff973e385351fbc
leejh1316 <89953872+leejh1316@users.noreply.github.com>
2024-10-23 11:06:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const script = () => {
+  const contentDom = document.getElementById("content");
+  const navItems = document.querySelectorAll(".nav-item > a");
+
+  const fetchPage = (url) => {
+    fetch(url)
+      .then((res) => res.text())
+      .then((data) => (contentDom.innerHTML = data));
+  };
+  const changeTitle = (value) => {
+    document.title = value;
+  };
+  navItems.forEach((item) =>
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      const url = item.getAttribute("href");
+      fetchPage(url);
+      changeTitle(item.innerHTML);
+    })
+  );
+
+  //default Page
+  fetchPage(navItems[0].getAttribute("href"));
+  changeTitle(navItems[0].innerHTML);
+};
+
+script();
